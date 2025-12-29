53d15b3b109a79f96b04ed355cec359f4283a737
Justin Carideo <justin.carideo@studio.unibo.it>
2025-12-29 15:53:30 +0100

    feat:extract loans and bookings for admin

--- view/admin-dashboard-view.php
+++ view/admin-dashboard-view.php
@@ -1,93 +1,63 @@
-
 <section>
     <header>
         <h1 class="text-center">Prenotazioni</h1>
         <img src="../svg/open-collapse.svg" alt="">
     </header>
     <ul>
-        <?php foreach($bookingRepo->findAll() as $booking) :?>
-        <li>
-            <form action="#">
-                <ul>
-                    <li>Studente:</li>
-                    <li>
-                    <?php
-                        $student = $studentRepo->findById($booking->getIdStudent());
-                        echo $student->getName(). " " . $student->getSurname();
-                    ?>
-                    </li>
-                    <li>Libro:</li>
-                    <li>Design Patterns</li>
-                    <li>Data:</li>
-                    <li>27/11/2025</li>
-                    <li>Copie disponibili:</li>
-                    <li>200</li>
-                </ul>
-                <footer>
-                    <input type="submit" value="Accetta"/>
-                </footer>
-            </form>
-        </li>
+        <?php
+        foreach ($bookingRepo->findAll() as $booking) :
+            $student = $studentRepo->findById($booking->getIdStudent());
+            $book = $bookRepo->findById($booking->getCodeBook());
+        ?>
+            <li>
+                <form action="#">
+                    <ul>
+                        <li>Studente:</li>
+                        <li><?php echo $student->getName() . " " . $student->getSurname(); ?> </li>
+                        <li>Libro:</li>
+                        <li><?php echo $book->getTitle(); ?></li>
+                        <li>Data:</li>
+                        <li><?php echo $booking->getDate(); ?></li>
+                        <li>Copie disponibili:</li>
+                        <li><?php echo $bookRepo->getAvailableCopiesCount($booking->getCodeBook()); ?></li>
+                    </ul>
+                    <footer>
+                        <input type="submit" value="Accetta" />
+                    </footer>
+                </form>
+            </li>
         <?php endforeach; ?>
-        <li>
-            <form action="#">
-                <ul>
-                    <li>Studente:</li>
-                    <li>Gianni Piergigio</li>
-                    <li>Libro:</li>
-                    <li>Design Patterns</li>
-                    <li>Data:</li>
-                    <li>27/11/2025</li>
-                    <li>Copie disponibili:</li>
-                    <li>200</li>
-                </ul>
-                <footer>
-                    <input type="submit" value="Accetta"/>
-                </footer>
-            </form>
-        </li>
-        <li>
-            <form action="#">
-                <ul>
-                    <li>Studente:</li>
-                    <li>Gianni Piergigio</li>
-                    <li>Libro:</li>
-                    <li>Design Patterns</li>
-                    <li>Data:</li>
-                    <li>27/11/2025</li>
-                    <li>Copie disponibili:</li>
-                    <li>200</li>
-                </ul>
-                <footer>
-                    <input type="submit" value="Accetta"/>
-                </footer>
-            </form>
-        </li>
     </ul>
 </section>
-<hr/>
+<hr />
 <section>
     <header>
         <h1 class="text-center">Prestiti</h1>
         <img src="../svg/open-collapse.svg" alt="">
     </header>
     <ul>
-        <li>
-            <form action="#">
-                <ul>
-                    <li>Studente:</li>
-                    <li>Gianni Piergigio</li>
-                    <li>Libro:</li>
-                    <li>Design Patterns</li>
-                    <li>Scadenza:</li>
-                    <li>27/11/2025</li>
-                    <li>Stato</li>
-                    <li>In restituzione</li>
-                </ul>
-                <footer>
-                    <input type="submit" value="Accetta"/>
-                </footer>
-            </form>
-        </li>
+        <?php
+        foreach ($loanRepo->findAll() as $loan) :
+            $student = $studentRepo->findById($loan->getIdStudent());
+            $book = $bookRepo->findById($loan->getCodeBook());
+        ?>
+            <li>
+                <form action="#">
+                    <ul>
+                        <li>Studente:</li>
+                        <li><?php echo $student->getName() . " " . $student->getSurname(); ?> </li>
+                        <li>Libro:</li>
+                        <li><?php echo $book->getTitle(); ?></li>
+                        <li>Scadenza:</li>
+                        <li><?php echo $loan->getRefundData(); ?></li>
+                        <li>Stato</li>
+                        <li><?php echo $loan->getState(); ?></li>
+                    </ul>
+                    <footer>
+                        <input type="submit" value="Accetta" />
+                    </footer>
+                </form>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </section>
