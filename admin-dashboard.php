c9ce8bebb1a4f05be4743418c4d18ad7381a35c9
Justin Carideo <justin.carideo@studio.unibo.it>
2025-12-29 12:08:12 +0100

    refact: admin css

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -4,6 +4,7 @@ require_once 'bootstrap.php';
 
 $templateParams["title"] = "Area Admin";
 $templateParams["content"] = "view/admin-dashboard-view.php";
+$templateParams["css"] = "style.css";
 require 'template/base.php';
 
 ?>
--- view/admin-dashboard-view.php
+++ view/admin-dashboard-view.php
@@ -1,86 +1,86 @@
 
 <section>
-                <header>
-                    <h1 class="text-center">Prenotazioni</h1>
-                    <img src="../svg/open-collapse.svg" alt="">
-                </header>
+    <header>
+        <h1 class="text-center">Prenotazioni</h1>
+        <img src="../svg/open-collapse.svg" alt="">
+    </header>
+    <ul>
+        <li>
+            <form action="#">
                 <ul>
-                    <li>
-                        <form action="#">
-                            <ul>
-                                <li>Studente:</li>
-                                <li>Gianni Piergigio</li>
-                                <li>Libro:</li>
-                                <li>Design Patterns</li>
-                                <li>Data:</li>
-                                <li>27/11/2025</li>
-                                <li>Copie disponibili:</li>
-                                <li>200</li>
-                            </ul>
-                            <footer>
-                                <input type="submit" value="Accetta"/>
-                            </footer>
-                        </form>
-                    </li>
-                    <li>
-                        <form action="#">
-                            <ul>
-                                <li>Studente:</li>
-                                <li>Gianni Piergigio</li>
-                                <li>Libro:</li>
-                                <li>Design Patterns</li>
-                                <li>Data:</li>
-                                <li>27/11/2025</li>
-                                <li>Copie disponibili:</li>
-                                <li>200</li>
-                            </ul>
-                            <footer>
-                                <input type="submit" value="Accetta"/>
-                            </footer>
-                        </form>
-                    </li>
-                    <li>
-                        <form action="#">
-                            <ul>
-                                <li>Studente:</li>
-                                <li>Gianni Piergigio</li>
-                                <li>Libro:</li>
-                                <li>Design Patterns</li>
-                                <li>Data:</li>
-                                <li>27/11/2025</li>
-                                <li>Copie disponibili:</li>
-                                <li>200</li>
-                            </ul>
-                            <footer>
-                                <input type="submit" value="Accetta"/>
-                            </footer>
-                        </form>
-                    </li>
+                    <li>Studente:</li>
+                    <li>Gianni Piergigio</li>
+                    <li>Libro:</li>
+                    <li>Design Patterns</li>
+                    <li>Data:</li>
+                    <li>27/11/2025</li>
+                    <li>Copie disponibili:</li>
+                    <li>200</li>
                 </ul>
-            </section>
-            <hr/>
-            <section>
-                <header>
-                    <h1 class="text-center">Prestiti</h1>
-                    <img src="../svg/open-collapse.svg" alt="">
-                </header>
+                <footer>
+                    <input type="submit" value="Accetta"/>
+                </footer>
+            </form>
+        </li>
+        <li>
+            <form action="#">
                 <ul>
-                    <li>
-                        <form action="#">
-                            <ul>
-                                <li>Studente:</li>
-                                <li>Gianni Piergigio</li>
-                                <li>Libro:</li>
-                                <li>Design Patterns</li>
-                                <li>Scadenza:</li>
-                                <li>27/11/2025</li>
-                                <li>Stato</li>
-                                <li>In restituzione</li>
-                            </ul>
-                            <footer>
-                                <input type="submit" value="Accetta"/>
-                            </footer>
-                        </form>
-                    </li>
+                    <li>Studente:</li>
+                    <li>Gianni Piergigio</li>
+                    <li>Libro:</li>
+                    <li>Design Patterns</li>
+                    <li>Data:</li>
+                    <li>27/11/2025</li>
+                    <li>Copie disponibili:</li>
+                    <li>200</li>
                 </ul>
-            </section>
+                <footer>
+                    <input type="submit" value="Accetta"/>
+                </footer>
+            </form>
+        </li>
+        <li>
+            <form action="#">
+                <ul>
+                    <li>Studente:</li>
+                    <li>Gianni Piergigio</li>
+                    <li>Libro:</li>
+                    <li>Design Patterns</li>
+                    <li>Data:</li>
+                    <li>27/11/2025</li>
+                    <li>Copie disponibili:</li>
+                    <li>200</li>
+                </ul>
+                <footer>
+                    <input type="submit" value="Accetta"/>
+                </footer>
+            </form>
+        </li>
+    </ul>
+</section>
+<hr/>
+<section>
+    <header>
+        <h1 class="text-center">Prestiti</h1>
+        <img src="../svg/open-collapse.svg" alt="">
+    </header>
+    <ul>
+        <li>
+            <form action="#">
+                <ul>
+                    <li>Studente:</li>
+                    <li>Gianni Piergigio</li>
+                    <li>Libro:</li>
+                    <li>Design Patterns</li>
+                    <li>Scadenza:</li>
+                    <li>27/11/2025</li>
+                    <li>Stato</li>
+                    <li>In restituzione</li>
+                </ul>
+                <footer>
+                    <input type="submit" value="Accetta"/>
+                </footer>
+            </form>
+        </li>
+    </ul>
+</section>
