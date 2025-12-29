244019606a0405c70ccb556018871ab9741d694a
Francesco Sacripante <francesco.sacripante@studio.unibo.it>
2025-12-29 01:46:31 +0100

    fix:navbar, removet unecesssary bootstrap

--- src/search-bar-toggle.js
+++ src/search-bar-toggle.js
@@ -1,15 +1,10 @@
 const searchButton = document.querySelector('#searchActivator');
 const searchCloseButton = document.querySelector('#searchCloseBtn');
-const leftContainer = document.querySelector('nav > div > div:first-child');
-const  middleContainer = document.querySelector('nav > div > div:nth-child(2)');
-const rightContainer = document.querySelector('#right-container');
+const leftContainer = document.querySelector('nav > div:first-child');
+
 searchButton.addEventListener('click', () => {
-    middleContainer.classList.remove('d-md-block');
-    rightContainer.classList.toggle('d-none');
     leftContainer.classList.toggle('d-none');
 });
 searchCloseButton.addEventListener('click', () => {
-    middleContainer.classList.add('d-md-block');
-    rightContainer.classList.toggle('d-none');
     leftContainer.classList.toggle('d-none');
 });
--- template/base.php
+++ template/base.php
@@ -12,160 +12,161 @@
 
 <body>
 
-      <nav class=" navbar  d-flex justify-content-center mx-0 mb-2 w-100 ">
-        <div class=" row d-flex justify-content-center align-items-center w-100 " style="min-height: 56px;">
-
-            <div class=" col container-fluid p-0 d-flex align-items-center ms-2 w-1">
-                <button class="navbar-toggler  d-md-none mx-0" type="button" data-bs-toggle="offcanvas"
+    <nav>
+        <div >
+            <!-- Sezione Sinistra: Logo e Hamburger -->
+            <div >
+                <button  type="button" data-bs-toggle="offcanvas"
                     data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span ></span>
                 </button>
-                <a href="index.php" class=" border border-0 text-decoration-none  p-0">
-                    <img src="/UniBook/svg/logo.svg" alt="" width="120" />
+                <a href="index.php" >
+                    <img src="/UniBook/svg/logo.svg" alt="UniBook Logo" width="120" />
                 </a>
             </div>
-            <div class="d-none d-md-block col ">
-                <ul class="navbar-nav flex-row justify-content-center align-items-center p-0 m-0">
-                    <li class="nav-item me-2">
-                        <a href="" class="nav-link">Negozio</a>
-                    </li>
-                    <li class="nav-item me-2">
-                        <a href="" class="nav-link">Negozio</a>
-                    </li>
-                    <li class="nav-item me-2">
-                        <a href="" class="nav-link">Negozio</a>
-                    </li>
+
+            <!-- Sezione Centro: Link Navigazione (desktop) -->
+            <div >
+                <ul >
+                    <li><a href="">Negozio</a></li>
+                    <li><a href="">Negozio</a></li>
+                    <li><a href="">Negozio</a></li>
                 </ul>
             </div>
 
+            <!-- Barra di Ricerca Collapsible -->
+           
 
-            <div class="collapse flex-grow-1 w-100" id="searchBar">
-                <form class=" d-flex">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success ms-2 my-2 my-sm-0" type="submit">Search</button>
-                    <button class="btn btn-outline-danger ms-2 my-2 my-sm-0" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#searchBar" aria-controls="searchBar" id="searchCloseBtn">Close</button>
-                </form>
-            </div>
-
-            <div id="right-container" class="col d-flex ms-auto justify-content-end align-items-center gap-2">
-                <div class="dropdown d-none d-md-block me-3">
-                    <img src="/UniBook/svg/menu-language-desktop-icon.svg" alt="" width="32" height="32"
-                        class="rounded-circle dropdown-toggle  " data-bs-toggle="dropdown" aria-expanded="false">
+            <!-- Sezione Destra: Icone e Azioni -->
+            <div >
+                <!-- Dropdown Lingua (desktop) -->
+                <div class="dropdown navbar-item desktop-only">
+                    <img src="/UniBook/svg/menu-language-desktop-icon.svg" alt="Language" width="32" height="32"
+                        class="rounded-circle dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                     <ul class="dropdown-menu dropdown-menu-end">
                         <li><a class="dropdown-item" href="#">Inglese</a></li>
-
                     </ul>
                 </div>
 
-                <a class="nav-link me-3  d-none d-md-block " id="dark-mode-switch" href="#">
-                    <img src="/UniBook/svg/dark-mode-desktop-icon.svg" alt="" width="24" height="24">
+                <!-- Dark Mode Toggle (desktop) -->
+                <a id="dark-mode-switch" href="#">
+                    <img src="/UniBook/svg/dark-mode-desktop-icon.svg" alt="Dark Mode" width="24" height="24">
                 </a>
-                <a class="nav-link me-3 " id="searchActivator" data-bs-toggle="collapse" data-bs-target="#searchBar"
-                    href="#">
-                    <img src="/UniBook/svg/search.svg" alt="" width="24" height="24">
+
+                <!-- Search Toggle -->
+                <a id="searchActivator" data-bs-toggle="collapse" data-bs-target="#searchBar" href="#">
+                    <img src="/UniBook/svg/search.svg" alt="Search" width="24" height="24">
                 </a>
+
+                <!-- User Profile / Login -->
                 <?php if ($sessionManager->isLogged()) : ?>
-                <div class="dropdown">
+                <div class="dropdown navbar-item">
                     <?php $student = $studentRepo->findById($_SESSION['userid']); ?>
-                    <img src="<?php echo UPLOAD_DIR . 'profile/' . htmlspecialchars($student->getProfileImage()); ?>" alt="" width="32" height="32"
-                        class="rounded-circle dropdown-toggle  " data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="<?php echo UPLOAD_DIR . 'profile/' . htmlspecialchars($student->getProfileImage()); ?>" 
+                        alt="Profile" width="32" height="32"
+                        class="rounded-circle dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                     <ul class="dropdown-menu dropdown-menu-end">
                         <li><a class="dropdown-item" href="#">Profile</a></li>
                         <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
+                        <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="logout.php">Logout</a></li>
                     </ul>
                 </div>
                 <?php else : ?>
-                <a class=" me-0 btn btn-danger" href="login-form.php">Login</a>
+                <a class="btn btn-danger" href="login-form.php">Login</a>
                 <?php endif; ?>
             </div>
+        </div>
 
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasLanguage">
-                <div class="offcanvas-header">
-                    <div class="d-flex align-items-center">
-                        <a class="btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
-                            <img src="/UniBook/svg/menu-back.svg" alt="">
-                        </a>
-                        <h5 class="offcanvas-title mx-3" id="offcanvasLanguageNavbarLabel">UniBook</h5>
-                    </div>
-                </div>
-                <div class="offcanvas-body d-flex flex-column justify-content-between mx-3 ">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link btn py-2 px-4 " aria-current="page" href="#">
-                                Inglese
-                            </a>
-                        </li>
-
-                    </ul>
+        <!-- Offcanvas Menu Mobile -->
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar">
+            <div class="offcanvas-header">
+                <div class="d-flex align-items-center">
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <h5 class="offcanvas-title mx-3">UniBook</h5>
                 </div>
             </div>
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar">
-                <div class="offcanvas-header">
-                    <div class="d-flex align-items-center">
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        <h5 class="offcanvas-title mx-3" id="offcanvasNavbarLabel">UniBook</h5>
-                    </div>
-                </div>
-                <div class="offcanvas-body d-flex flex-column justify-content-between mx-3 ">
-                    <ul class="navbar-nav  flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link py-2 px-4" aria-current="page" href="#">
-                                <div class="d-flex flex-row align-items-center ">
-                                    <img src="/UniBook/svg/shop.svg" alt="" class="" width="48" height="48" />
-                                    <span>Negozio</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link btn py-2 px-4 " aria-current="page" href="#">
-                                <div class="d-flex flex-row align-items-center ">
-                                    <img src="/UniBook/svg/shop-black.svg" alt="" class="" width="48" height="48" />
-                                    <span>Negozio</span>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
+            <div class="offcanvas-body d-flex flex-column justify-content-between mx-3">
+                <ul class="navbar-nav flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link py-2 px-4" href="#">
+                            <div class="d-flex flex-row align-items-center">
+                                <img src="/UniBook/svg/shop.svg" alt="" width="48" height="48" />
+                                <span>Negozio</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link py-2 px-4" href="#">
+                            <div class="d-flex flex-row align-items-center">
+                                <img src="/UniBook/svg/shop-black.svg" alt="" width="48" height="48" />
+                                <span>Negozio</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
 
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link btn py-2 px-4 " data-bs-toggle="offcanvas"
-                                data-bs-target="#offcanvasLanguage" aria-controls="offcanvasLanguage"
-                                aria-current="page" href="#">
-                                <div class="d-flex flex-row align-items-center ">
-                                    <img src="/UniBook/svg/shop-black.svg" alt="" class="" width="48" height="48" />
-                                    <span>Cambia Lingua</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link btn py-2 px-4 " aria-current="page" href="#">
-                                <div class="d-flex flex-row align-items-center ">
-                                    <img src="/UniBook/svg/shop-black.svg" alt="" class="" width="48" height="48" />
-                                    <span>Cambia tema</span>
-                                </div>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link btn py-2 px-4 " aria-current="page" href="#">
-                                <div class="d-flex flex-row align-items-center ">
-                                    <img src="/UniBook/svg/shop-black.svg" alt="" class="" width="48" height="48" />
-                                    <?php if ($sessionManager->isLogged()) : ?>
-                                    <span>Log out</span>
-                                    <?php else : ?>
-                                    <span>Log in</span>
-                                    <?php endif; ?>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link py-2 px-4" data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasLanguage" aria-controls="offcanvasLanguage" href="#">
+                            <div class="d-flex flex-row align-items-center">
+                                <img src="/UniBook/svg/shop-black.svg" alt="" width="48" height="48" />
+                                <span>Cambia Lingua</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link py-2 px-4" href="#">
+                            <div class="d-flex flex-row align-items-center">
+                                <img src="/UniBook/svg/shop-black.svg" alt="" width="48" height="48" />
+                                <span>Cambia tema</span>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link py-2 px-4" href="#">
+                            <div class="d-flex flex-row align-items-center">
+                                <img src="/UniBook/svg/shop-black.svg" alt="" width="48" height="48" />
+                                <?php if ($sessionManager->isLogged()) : ?>
+                                <span>Log out</span>
+                                <?php else : ?>
+                                <span>Log in</span>
+                                <?php endif; ?>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
+
+        <!-- Offcanvas Lingua -->
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasLanguage">
+            <div class="offcanvas-header">
+                <div class="d-flex align-items-center">
+                    <a class="btn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                        <img src="/UniBook/svg/menu-back.svg" alt="">
+                    </a>
+                    <h5 class="offcanvas-title mx-3">UniBook</h5>
+                </div>
+            </div>
+            <div class="offcanvas-body d-flex flex-column justify-content-between mx-3">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link py-2 px-4" href="#">Inglese</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+         <div class="collapse search-collapse" id="searchBar">
+                <form class="search-form">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search" />
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    <button class="btn btn-outline-danger" id="searchCloseBtn" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#searchBar" aria-controls="searchBar">Close</button>
+                </form>
+            </div>
     </nav>
 
     <main>
